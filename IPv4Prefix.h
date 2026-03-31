2f9bc9987c35a0adadfedc52e70ecca8bf8764c9
Viacheslav Pihida <vyacheslav.pigida@gmail.com>
2026-03-31 22:08:27 +0100

    02: I finally understood masks!

---
+++ IPv4Prefix.h
@@ -0,0 +1,26 @@
+#include <cstdint>
+
+class IPv4Prefix{
+
+public:
+    uint32_t base;
+    uint8_t maskLength;
+
+    IPv4Prefix();
+    IPv4Prefix(uint32_t base, uint8_t maskLength):
+        base(base), maskLength(maskLength){};
+
+    bool operator==(const IPv4Prefix& other){
+        return other.base == base && other.maskLength == maskLength;
+    }
+
+    uint32_t getMask() const{
+        return (0xFFFFFFFF << (32-maskLength)) % 0x100000000;
+
+    }
+
+    uint32_t masked() const{
+        return base & 
+    }
+
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 
 
 #include "Set.h"
+#include "IPv4Prefix.h"
 int main(){
-    Set<char*> set;
+
+    IPv4Prefix pref(0x23531232, 16);
+    std::cout<<std::hex<<pref.base<<std::endl;
+    std::cout<<std::hex<<pref.getMask()<<std::endl;
+    std::cout<<std::hex<<pref.masked()<<std::endl;
 
     return EXIT_SUCCESS;
 }
